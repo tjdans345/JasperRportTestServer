9f75377907779a417a136ed0fe41df715772227f
tjdans345 <tjdans34568@gmail.com>
2022-02-20 21:13:15 +0900

    pdf, html 파일로 다운은 했지만 데이터 바인딩 안됨 -> 해결책 찾아야함

--- src/main/java/com/example/demo/jasper/controller/JasperApiController.java
+++ src/main/java/com/example/demo/jasper/controller/JasperApiController.java
@@ -3,10 +3,13 @@ package com.example.demo.jasper.controller;
 import com.example.demo.jasper.domain.entity.MemberEntity;
 import com.example.demo.jasper.service.JasperService;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,6 +22,11 @@ public class JasperApiController {
     @GetMapping
     public List<MemberEntity> getMemberList() {
         return jasperService.getMemberList();
+    }
+
+    @GetMapping("/{format}")
+    public String generateReport(@PathVariable String format) throws JRException, FileNotFoundException {
+       return jasperService.exportReport(format);
     }
 
 }
--- src/main/java/com/example/demo/jasper/service/JasperService.java
+++ src/main/java/com/example/demo/jasper/service/JasperService.java
@@ -4,15 +4,47 @@ package com.example.demo.jasper.service;
 import com.example.demo.jasper.domain.entity.MemberEntity;
 import com.example.demo.jasper.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class JasperService {
 
     private final MemberRepository memberRepository;
+
+    public String exportReport(String reportFormat) throws JRException, FileNotFoundException {
+        var path = "C:\\Users\\user\\Desktop\\reporttest";
+        var members = memberRepository.findAll();
+        // load file and compile it
+        File file = ResourceUtils.getFile("classpath:members.jrxml"); // 이 파일안에 데이터가 어떻게 매핑되는지를 먼저 파악하기
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(members);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Java Meteor");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters , dataSource);
+
+        // equalsIgnoreCase 대소문자 구분없이 비교함
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path+"\\members.html");
+        }
+
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path+"\\members.pdf");
+        }
+
+        return "report generated in path : " + path;
+
+    }
 
     public List<MemberEntity> getMemberList() {
         return memberRepository.findAll();
